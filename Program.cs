99b918d6c01467e08655c55a382df6d9519fbf96
Nikita <topolik777@mail.ru>
2024-01-26 00:52:56 +0300

    Создал ещё один массив и цикл для переборки элементов массива- надо доработать

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿string[] StringsArray = { "GeekBrains", "HelloWorld", "abc", "hjiufhwei", "fjfue", "12f", "fsd", "ffdfew", "124" };
+﻿
+string[] StringsArray = { "GeekBrains", "HelloWorld", "abc", "hjiufhwei", "fjfue", "12f", "fsd", "ffdfew", "124" };
+
+ private string[] SortArray(string[] strings){
+ string[] NewArray = new string[strings.Length];
+
+for (int i = 0; i < strings.Length; i++)
+{
+    if(strings[i].Length <= 3)
+    {
+         NewArray[i] = strings[i];
+    }
+}
+
+}
+
