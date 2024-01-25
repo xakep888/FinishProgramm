2a5cb050d1ab8d4f2e4e9908f79fa795dee6cccd
Nikita <topolik777@mail.ru>
2024-01-26 02:00:35 +0300

    На всякий случай

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApplication2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] StringsArray = { "GeekBrains", "HelloWorld", "abc", "hjiufhwei", "ffe", "12f", "fsd", "ffdfew", "124" };
-            string[] NewArray = StringsArray.Where(s => s.Length <= 3).ToArray();
+            string[] NewArray = StringsArray.Where(f => f.Length <= 3).ToArray();
             Console.Write(string.Join(",", NewArray));
             
         }
